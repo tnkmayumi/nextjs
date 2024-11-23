a3e5ffccef3a85e29550e05212c0a1b97f4b9df3
tnkmayumi <miiy.16f@gmail.com>
2024-11-23 14:04:55 +0900

    commit

--- src/api.ts
+++ src/api.ts
@@ -1,48 +1,48 @@
 import { Task } from "./types";
 
-export const getAllTodos = async(): Promise<Task[]> => {
-    const res = await fetch(`http://localhost:3001/tasks`,{
-        cache: "no-store", //SSR
-    });
-    const todos = res.json();
+export const getAllTodos = async (): Promise<Task[]> => {
+  const res = await fetch(`http://localhost:3001/tasks`, {
+    cache: "no-store", //SSR
+  });
+  const AddTodo = res.json();
 
-    return todos;
+  return AddTodo;
 };
 
-export const addTodo = async(todo: Task): Promise<Task> => {
-    const res = await fetch(`http://localhost:3001/tasks`,{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(todo),
-    });
-    const newTodo = res.json();
+export const addTodo = async (todo: Task): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const newTodo = res.json();
 
-    return newTodo;
+  return newTodo;
 };
 
-export const editTodo = async(id: string, newText: string): Promise<Task> => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`,{
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text: newText }),
-    });
-    const updatedTodo = res.json();
+export const editTodo = async (id: string, newText: string): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: newText }),
+  });
+  const updatedTodo = res.json();
 
-    return updatedTodo;
+  return updatedTodo;
 };
 
-export const deleteTodo = async(id: string): Promise<Task> => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`,{
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const deleteTodo = res.json();
+export const deleteTodo = async (id: string): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/tasks/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const deleteTodo = res.json();
 
-    return deleteTodo;
+  return deleteTodo;
 };
